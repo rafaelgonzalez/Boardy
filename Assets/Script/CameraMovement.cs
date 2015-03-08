fa32a260e51dd3aaf29c6cf5b4fc05472817ab63
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2015-03-09 01:00:40 +1100

    Refactor CameraMovement

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -3,37 +3,56 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
+	public Vector3 relativePosition = new Vector3(0, 7, -7);
+
 	private int cameraFocusIndex = 0;
 	private BoardManager boardManager;
 
 	void Start () {
 		boardManager = transform.parent.GetComponent<BoardManager>();
 
-		SnapToCharacter(boardManager.playerCharacters[cameraFocusIndex]);
+		SetInitialPosition();
 	}
 
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Tab)) {
-			cameraFocusIndex = cameraFocusIndex + 1;
-			if (cameraFocusIndex >= boardManager.playerCharacters.Count) {
-				cameraFocusIndex = 0;
-			}
-			
-			SnapToCharacter(boardManager.playerCharacters[cameraFocusIndex]);
+			ChangeCharacterFocus(1);
 		}
 	}
 
-	public void SnapToCharacter(GameObject character) {
-		transform.position = character.transform.position;
-		transform.rotation = Quaternion.identity;
+	private void SetInitialPosition () {
+		transform.position = Vector3.zero;
+		transform.position = CurrentCharacter().transform.position + relativePosition;
 		
-		transform.parent = character.transform;
-		
-		Vector3 movement = new Vector3(0, 7, -7);
-		transform.Translate(movement);
-		
-		Vector3 lookDirection = character.transform.position - GetComponent<Camera>().transform.position;
+		Vector3 lookDirection = CurrentCharacter().transform.position - transform.position;
 		Quaternion rotation = Quaternion.LookRotation(lookDirection);
+		
 		transform.rotation = rotation;
+		transform.parent = CurrentCharacter().transform;
+	}
+
+	private void ChangeCharacterFocus(int indexChange) {
+		GameObject oldCharacter = boardManager.playerCharacters[cameraFocusIndex];
+
+		cameraFocusIndex = cameraFocusIndex + indexChange;
+
+		if (cameraFocusIndex >= boardManager.playerCharacters.Count)
+			cameraFocusIndex = 0;
+		else if (cameraFocusIndex < 0)
+			cameraFocusIndex = boardManager.playerCharacters.Count - 1;
+
+		if (oldCharacter != CurrentCharacter())
+			SnapToCharacter(oldCharacter, CurrentCharacter());
+	}
+
+	private void SnapToCharacter(GameObject oldCharacter, GameObject newCharacter) {
+		transform.parent = newCharacter.transform;
+
+		Vector3 endPosition = Vector3.zero + newCharacter.transform.position + relativePosition;
+		transform.position = endPosition;
+	}
+
+	private GameObject CurrentCharacter() {
+		return boardManager.playerCharacters[cameraFocusIndex];
 	}
 }
